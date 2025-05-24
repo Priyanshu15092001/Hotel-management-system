61f59d177cdb4d3b64409e1236d2315e7ce18894
Priyanshu Ghosh <priyanshughosh2001.pg@gmail.com>
2025-05-24 22:23:33 +0530

    Adding source code for admin menu page

--- frontend-admin/src/components/Sidebar/Sidebar.jsx
+++ frontend-admin/src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import analyticsImg from "../../assets/sidebar/analytics.svg";
 import menuImg from "../../assets/sidebar/menu.svg";
 import orderImg from "../../assets/sidebar/orders.svg";
 import seatImg from "../../assets/sidebar/seats.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 export default function Sidebar() {
   const links = [
     { path: "/", icon: analyticsImg },
@@ -12,19 +12,22 @@ export default function Sidebar() {
     { path: "/orders", icon: orderImg },
     { path: "/menu", icon: menuImg },
   ];
-
+  const location = useLocation();
   return (
     <div className={styles.leftbar}>
       <div className={styles.circle}></div>
       <div className={styles.sidebar}>
         <div className={styles.navlink}>
-          {links.map((link, index) => (
-            <Link to={link.path} key={index}>
-              <div className={styles.icon}>
+          {links.map((link, index) => {
+
+             const isActive = location.pathname === link.path;
+
+            return <Link to={link.path} key={index}>
+              <div className={styles.icon} style={isActive?{backgroundColor:"#d9d9d9"}:{}}>
                 <img src={link.icon} alt="" />
               </div>
-            </Link>
-          ))}
+            </Link>;
+          })}
         </div>
         <div className={styles.innerCircle}></div>
       </div>
